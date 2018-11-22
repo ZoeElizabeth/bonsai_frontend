6b04e8cc6f93cd8a94f24cba63a6ad35cf1e3714
ZoeElizabeth <zoe.gelech@gmail.com>
2018-11-21 17:27:44 -0800

    fisrt

--- pages/OverviewView.js
+++ pages/OverviewView.js
@@ -1,20 +1,46 @@
 import React from 'react';
 import { SectionList, Image, StyleSheet, Text, View } from 'react-native';
-import { Constants } from 'expo';
+import { Constants, SvgEllipseProps } from 'expo';
+import { AreaChart, Grid, ProgressCircle } from 'react-native-svg-charts'
+import PropTypes from 'prop-types';
 
-class AchievmentsView extends React.Component {
+export default class ProcessGraph extends React.Component {
   render() {
-    return ( 
-      <Text>
-        Hello
-      </Text>
-    );
+    return(
+        <View style={{ padding: this.props.padding, width: this.props.size + (this.props.padding * 2) }}>
+          <View>
+            <ProgressCircle
+              strokeWidth={this.props.stroke}
+              style={{ height: this.props.size }}
+              progress={this.props.result/this.props.total}
+              progressColor={this.props.color}
+              startAngle={3.5 * Math.PI / 3}
+              endAngle={8.5 * Math.PI / 3}
+            />
+          </View>
+          <View style={{ position: 'absolute', bottom: 0, left: this.props.padding, width: this.props.size }}>
+            <Text style={{ fontSize: 12, textAlign: 'center', fontWeight: 'bold' }}>Result</Text>
+            <Text style={{ fontSize: 18, textAlign: 'center', fontWeight: 'bold' }}>{this.props.result}/{this.props.total}</Text>
+          </View>
+        </View>
+        );
+      }
+}
 
-  }
+ProcessGraph.propTypes = {
+  result: PropTypes.number.isRequired,
+  total: PropTypes.number.isRequired,
+  padding: PropTypes.number,
+  size: PropTypes.number,
+  stroke: PropTypes.number,
+  color: PropTypes.string
 };
 
-const styles = StyleSheet.create({
-
-});
-
-export default AchievmentsView;
+ProcessGraph.defaultProps = {
+  result: 10,
+  total: 30,
+  padding: 10,
+  size: 200,
+  stroke: 8,
+  color: 'black'
+};
