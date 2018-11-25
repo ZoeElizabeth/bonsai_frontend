0e3ec1e87ddf693df2a2a4e7098d936a71f8906c
ZoeElizabeth <zoe.gelech@gmail.com>
2018-11-25 13:26:43 -0800

    fixed splash added stickers file

--- modal/Splash.js
+++ modal/Splash.js
@@ -1,21 +1,33 @@
 import React, {Component} from 'react';
-import {Modal, TouchableHighlight, View, Alert, StyleSheet, ImageBackground, Image} from 'react-native';
+import { Modal, TouchableHighlight, View, Alert, StyleSheet, ImageBackground, Image} from 'react-native';
 import { Content, Text, Form, Item, Input, Icon} from 'native-base';
 import RadioGroup from 'react-native-radio-buttons-group';
 import bg from '../emoji/emoji.js';
+import { Font, AppLoading } from 'expo';
 
 export default class Splash extends Component {
   state = {
     modalVisible: true,
     username: '',
     password:'',
+    fontLoaded: false,
   };
 
   setModalVisible(visible) {
     this.setState({modalVisible: visible});
   }
 
+  async componentDidMount() {
+    Font.loadAsync({
+
+      'GiveYouGlory': require('../assets/fonts/GiveYouGlory.ttf'),
+    });   this.setState({ fontLoaded: true });
+    console.log('wtf')
+  }
+  
+  
   render() {
+
     return (
       <View>
      
@@ -30,7 +42,7 @@ export default class Splash extends Component {
           <ImageBackground style={{width: '100%', height: '100%'}} source={bg.bg}>
             <View style={styles.title}>
           
-              <Text>Welcome to Bonsai</Text>
+              <Text style={styles.text}>Welcome to Bonsai</Text>
             </View>
               
             <View style={styles.form}>
@@ -59,7 +71,7 @@ export default class Splash extends Component {
                     this.setModalVisible(false);
                   }}>
 
-                  <Text style={{color: '#538B9C'}}>Login</Text>
+                  <Text style={{color: '#fff', fontWeight: 'bold'}}>Login</Text>
 
                 </TouchableHighlight>
 
@@ -68,7 +80,7 @@ export default class Splash extends Component {
                   onPress={() => {
                     this.setModalVisible(false);
                   }}>
-                  <Text style={{color: '#538B9C'}}>Register</Text>
+                  <Text style={{color: '#fff', fontWeight: 'bold'}}>Register</Text>
 
                 </TouchableHighlight>
               </View>
@@ -87,14 +99,17 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     alignItems: 'center',
     fontSize: 35,
+
   },
+text: {
+  // fontFamily: 'GiveYouGlory',
+},
   title: {
     // flex: 1,
 
     textAlign: 'center',
     alignItems: 'center',
     marginTop: 100,
-  
     fontSize: 35,
   },
   button: {
